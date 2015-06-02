ced9489644d081d2b7bab9179efe84c21cb35adc
Matt Fair <matt.fair@gmail.com>
2015-06-01 23:32:42 -0400

    switch to dynamic SVG cards using Poker.JS

--- client/app/shared/directives/board/board.directive.js
+++ client/app/shared/directives/board/board.directive.js
@@ -9,33 +9,49 @@ angular.module('handDbApp')
 
       }
 
+      function getSuite(c){
+        switch(c){
+          case 'h':
+            return 'hearts';
+          case 'd':
+            return 'diamonds';
+          case 'c':
+            return 'clubs';
+          case 's':
+            return 'spades';
+        }
+
+        return '';
+      }
+
       $scope.updateBoard = function(){
-        var boardCards = $scope.values.toUpperCase().split(',');
+        var boardCards = $scope.values.toLowerCase().split(',');
 
         //turn over all cards
-        $scope.flop_card1 = 'assets/images/cards/Red_Back.svg';
-        $scope.flop_card2 = 'assets/images/cards/Red_Back.svg';
-        $scope.flop_card3 = 'assets/images/cards/Red_Back.svg';
-        $scope.turn_card = 'assets/images/cards/Red_Back.svg';
-        $scope.river_card = 'assets/images/cards/Red_Back.svg';
+        $scope.flop_card1 = Poker.getBackData(120, '#b55', '#a22');
+        $scope.flop_card2 = Poker.getBackData(120, '#b55', '#a22');
+        $scope.flop_card3 = Poker.getBackData(120, '#b55', '#a22');
+        $scope.turn_card = Poker.getBackData(120, '#b55', '#a22');
+        $scope.river_card = Poker.getBackData(120, '#b55', '#a22');
 
         if($scope.values.length >0) {
           for (var i = 0; i < boardCards.length; i++) {
+            var card = Poker.getCardData(120, getSuite(boardCards[i][1]), boardCards[i][0]);
             switch (i) {
               case 0:
-                $scope.flop_card1 = 'assets/images/cards/' + boardCards[i] + '.svg';
+                $scope.flop_card1 = card;
                 break;
               case 1:
-                $scope.flop_card2 = 'assets/images/cards/' + boardCards[i] + '.svg';
+                $scope.flop_card2 = card;
                 break;
               case 2:
-                $scope.flop_card3 = 'assets/images/cards/' + boardCards[i] + '.svg';
+                $scope.flop_card3 = card;
                 break;
               case 3:
-                $scope.turn_card = 'assets/images/cards/' + boardCards[i] + '.svg';
+                $scope.turn_card = card;
                 break;
               case 4:
-                $scope.river_card = 'assets/images/cards/' + boardCards[i] + '.svg';
+                $scope.river_card = card;
                 break;
             }
           }
